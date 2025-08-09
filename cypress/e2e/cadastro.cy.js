ef021cd3ff1ece0a15c69e605d1ee56db9ce2826
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2025-08-08 22:30:12 -0300

    configuração do cypress cloud

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,10 +1,15 @@
 /// <reference types="cypress"/>
 import { faker } from '@faker-js/faker';
+import cadastroPage from '../support/pages/cadastro-page';
 
 describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
 
     beforeEach(() => {
-        cy.visit('register.html')
+        cadastroPage.visitarPaginaCadastro()
+    });
+
+    afterEach(() => {
+        cy.screenshot()
     });
 
     it('Deve fazer cadastro com sucesso, usando função JS', () => {
@@ -38,6 +43,17 @@ describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
         let nome = faker.person.fullName({ sex: 'male' })
         cy.preencherCadastro(nome ,email, '1198765432165', 'Teste@123', 'Teste@123')
         cy.url().should('include', 'dashboard')
-    });    
+    }); 
+    
+    it('Deve fazer cadastro com sucesso - Usando Page Objects', () => {
+        let email = `teste${Date.now()}@teste.com`
+        cadastroPage.preencherCadastro('Fabio Araújo', email, '11987654321', 'senha123', 'senha123')
+        cy.url().should('include', 'dashboard')
+    });
+
+    it('Deve validar mensagem ao tentar cadastrar sem preencher nome', () => {
+        cadastroPage.preencherCadastro('', 'fabio@teste.com', '11987654321', 'senha123', 'senha123')
+        cy.get(':nth-child(1) > .invalid-feedback').should('contain', 'Nome deve ter pelo menos 2 caracteres')
+    });
 
 });
